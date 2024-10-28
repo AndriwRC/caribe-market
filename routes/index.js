a74a17d1e74ae6e65282a3684fad123edae40293
AndriwRC <randriw4@gmail.com>
2024-10-27 23:45:31 -0500

    set routes structure

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,8 @@
 const { Router } = require('express');
+const productsRouter = require('./products.router');
+const categoriesRouter = require('./categories.router');
+const storesRouter = require('./stores.router');
+const usersRouter = require('./users.router');
 
 function routerApi(app) {
   const router = Router();
@@ -6,6 +10,10 @@ function routerApi(app) {
   router.get('/', (req, res) => {
     res.send('Welcome to Caribe Market api version: 1');
   });
+  router.use('/products', productsRouter);
+  router.use('/categories', categoriesRouter);
+  router.use('/stores', storesRouter);
+  router.use('/users', usersRouter);
 }
 
 module.exports = routerApi;
